43575b827b3806823c5a5c20398283acb627f6ae
Ahamed Imran <immy123@gmail.com>
2017-04-22 22:53:31 +0530

    prevent remote call if search query empty

--- app/components/user/UserListContainer.js
+++ app/components/user/UserListContainer.js
@@ -5,18 +5,31 @@ class UserListContainer extends React.Component {
 
   componentDidUpdate(previousProps, previousState) {
     if (this.props.query !== previousProps.query) {
-      this.fetchUserList().then(data => {
-        // update the UserList props to rerender with the new user list
-        this.userList = data.items;
-
-        // update the state in the App component for further usage
-        this.props.onUserListUpdate(this.userList);
-      }).catch(error => {
-        console.error(`Promise error due to network request failure - ${error}`);
-      });
+      // check if query is empty
+      // update as blank array if empty
+      // fetch data if not empty
+      if (this.props.query === '') {
+        this.updateUserList();
+      } else {
+        this.fetchUserList().then(data => {
+          this.updateUserList(data.items);
+        }).catch(error => {
+          console.error(`Promise error due to network request failure - ${error}`);
+        });
+      }
     }
   }
 
+  // update local user list
+  updateUserList(userList = []) {
+    // update the UserList props to rerender with the new user list    
+    this.userList = userList;
+
+    // update the state in the App component for further usage    
+    this.props.onUserListUpdate(this.userList);
+  }
+
+  // fetch remote user list data from github
   fetchUserList(query) {
     const apiUrl = `https://api.github.com/search/users?q=${this.props.query}`;
 
