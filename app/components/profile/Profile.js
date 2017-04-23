43df6332a5037c10594b11d6d2930e47f9477a14
Ahamed Imran <immy123@gmail.com>
2017-04-23 13:15:34 +0530

    basic implementation of profile

--- app/components/profile/Profile.js
+++ app/components/profile/Profile.js
@@ -1,7 +1,20 @@
-const Profile = ({ user }) => {
-  return React.createElement(
-    'section',
-    null,
-    []
-  );
+const Profile = ({ match, userList }) => {
+  let currentUser = {};
+
+  // store param ID and cast to int
+  const id = +match.params.id;
+
+  userList.some((user) => {
+    if (user.id === id) {
+      currentUser = user;
+      return;
+    }
+  });
+
+  const userDetailElements = [
+    React.createElement('li', null, `User ID: ${currentUser.id}`),
+    React.createElement('li', null, `Username: ${currentUser.login}`)
+  ];
+
+  return React.createElement('section', null, userDetailElements);
 };
