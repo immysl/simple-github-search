f9d06589e7b105f13be0557b2465ca2464230037
Ahamed Imran <immy123@gmail.com>
2017-04-23 21:39:42 +0530

    display user info and repo details in profile

--- app/components/profile/Profile.js
+++ app/components/profile/Profile.js
@@ -1,16 +1,43 @@
 const Profile = ({ userInfo, userRepos }) => {
-  let userDetailElements = [];
+  let userInfoElements = [];
+  let userReposElements = [];
 
   // avoid undefined getting printed on screen
   // the userInfo obj is returned asynchronously
   // so only render child components when it is available
   if (userInfo && userInfo.id) {
-    userDetailElements = [
-      React.createElement('li', null, `User ID: ${userInfo.id}`),
-      React.createElement('li', null, `Username: ${userInfo.login}`),
-      React.createElement('li', null, `Name: ${userInfo.name}`)
-    ];
+    for (const prop in userInfo) {
+      if (userInfo.hasOwnProperty(prop) === true) {
+        userInfoElements.push(
+          h('li', null, `${prop}: ${userInfo[prop]}`)
+        );
+      }
+    }
   }
 
-  return React.createElement('section', null, userDetailElements);
+  // check for the same reason as above
+  if (userRepos && userRepos.length > 0) {
+    userReposElements = userRepos.map(repo => {
+      return h(
+        'li',
+        null,
+        [
+          h('a', { href: repo.html_url }, `${repo.name}: `),
+          h('span', null, repo.description)
+        ]
+      );
+    });
+  }
+
+  return h(
+    'section',
+    null,
+    [
+      h(ReactRouterDOM.Link, { to: '/' }, 'Go Back'),
+      h('h2', null, 'User Info'),
+      h('ul', { className: 'list-unstyled' }, userInfoElements),
+      h('h2', null, 'User Repos'),
+      h('ul', { className: 'list-unstyled' }, userReposElements)
+    ]
+  );
 };
