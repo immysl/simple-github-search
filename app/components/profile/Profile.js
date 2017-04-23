37ca61eee778e942c86ecb4c6e535e7c35504aa9
Ahamed Imran <immy123@gmail.com>
2017-04-23 15:46:09 +0530

    prevent profile from rendering child components if undefined

--- app/components/profile/Profile.js
+++ app/components/profile/Profile.js
@@ -1,9 +1,16 @@
 const Profile = ({ userInfo, userRepos }) => {
-  const userDetailElements = [
-    React.createElement('li', null, `User ID: ${userInfo.id}`),
-    React.createElement('li', null, `Username: ${userInfo.login}`),
-    React.createElement('li', null, `Name: ${userInfo.name}`)    
-  ];
+  let userDetailElements = [];
+
+  // avoid undefined getting printed on screen
+  // the userInfo obj is returned asynchronously
+  // so only render child components when it is available
+  if (userInfo && userInfo.id) {
+    userDetailElements = [
+      React.createElement('li', null, `User ID: ${userInfo.id}`),
+      React.createElement('li', null, `Username: ${userInfo.login}`),
+      React.createElement('li', null, `Name: ${userInfo.name}`)
+    ];
+  }
 
   return React.createElement('section', null, userDetailElements);
 };
