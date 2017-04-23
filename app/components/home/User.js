37dad86e096fbad343e5a9edf6502c43f1f6deb8
Ahamed Imran <immy123@gmail.com>
2017-04-23 08:29:29 +0530

    added router link to user in user list

---
+++ app/components/home/User.js
@@ -0,0 +1,14 @@
+const User = ({ user }) => {
+  return React.createElement(
+    'li',
+    null,
+    React.createElement(
+      ReactRouterDOM.Link,
+      { to: `profile/${user.id}` },
+      [
+        React.createElement('span', null, user.id),
+        React.createElement('img', { src: user.avatar_url, width: 100 }, null)
+      ]
+    )
+  );
+};
